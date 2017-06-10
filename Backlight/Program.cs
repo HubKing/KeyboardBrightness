abbe813e25828738a23281bbf43e3bba818db4d9
GitHub King <typingcat@gmail.com>
2017-06-11 00:04:56 +0900

    Add files via upload

---
+++ Backlight/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Principal;
+
+namespace Backlight
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                var isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+                if(!isAdmin)
+                {
+                    Console.WriteLine("Not run as administrator. Set functions will not work.");
+                }
+            }
+            var dllPath = ConfigurationManager.AppSettings["DllPath"];
+            if(!File.Exists(dllPath))
+            {
+                Console.WriteLine($"Samsung keyboard DLL does not exist at {dllPath}. Set the correct path in the App.config file.");
+                return -1;
+            }
+
+            var kbapi = new SamsungKBAPI(dllPath);
+            kbapi.Initialise();
+
+            if(args.Length == 1)
+            {
+                int brightness = int.Parse(args[0]);
+                SetBrightness(kbapi, brightness);
+            }
+            else
+            {
+                DisplayInfo(kbapi);
+            }
+
+            return 0;
+        }
+
+        static void SetBrightness(SamsungKBAPI kbapi, int brightness)
+        {
+            var result = kbapi.SetBrightness(brightness);
+            Console.WriteLine($"SetBrightness({brightness}) returned {result}.");
+            brightness = kbapi.GetCurrentBrightness();
+            Console.WriteLine("Current brightness: " + brightness);
+        }
+
+        static void DisplayInfo(SamsungKBAPI kbapi)
+        {
+            var t = kbapi.GetKeboardType();
+            Console.WriteLine("Keyboard type: " + t);
+            var als = kbapi.GetALSExistence();
+            Console.WriteLine("Automatic light sensor: " + als);
+            var alsStatus = kbapi.GetALSStatus();
+            Console.WriteLine("Automatic light sensor status: " + alsStatus);
+            var max = kbapi.GetMaximumBrightness();
+            Console.WriteLine("Maximum supported brightness: " + max);
+            var brightness = kbapi.GetCurrentBrightness();
+            Console.WriteLine("Current brightness: " + brightness);
+        }
+    }
+}
---
+++ Backlight/SamsungKBAPI.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Backlight
+{
+    class SamsungKBAPI
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        IntPtr DllModule;
+
+        public SamsungKBAPI(string dllPath)
+        {
+            var dir = Path.GetDirectoryName(dllPath);
+            var dll = Path.GetFileName(dllPath);
+
+            /* The keyboard library requires other DLL files in the same directory,
+               so just calling SetDllDirectory() did not work.
+             */
+            Environment.CurrentDirectory = dir;
+            DllModule = LoadLibrary(dllPath);
+            if (DllModule == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        Dictionary<string, Delegate> FunctionCache = new Dictionary<string, Delegate>();
+        Delegate GetFunction<T>(string name)
+        {
+            Delegate func = null;
+
+            if(!FunctionCache.TryGetValue(name, out func))
+            {
+                var functionAddress = GetProcAddress(DllModule, name);
+                func = Marshal.GetDelegateForFunctionPointer(functionAddress, typeof(T));
+                FunctionCache[name] = func;
+            }
+
+            return func;
+        }
+
+        #region Delegates
+        delegate void Arg0Ret0();
+        delegate int Arg0RetInt();
+
+        //https://stackoverflow.com/questions/3307000/calling-a-c-function-from-c-sharp-unbalanced-stack
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int ArgIntRetInt(int arg0);
+
+        void RunArg0Ret0(string name)
+        {
+            var func = GetFunction<Arg0RetInt>(name);
+            func.DynamicInvoke();
+        }
+
+        int RunArg0RetInt(string name)
+        {
+            var func = GetFunction<Arg0RetInt>(name);
+            return (int)func.DynamicInvoke();
+        }
+
+        int RunArgIntRetInt(string name, int arg0)
+        {
+            var func = GetFunction<ArgIntRetInt>(name);
+            return (int)func.DynamicInvoke(arg0);
+        }
+        #endregion
+
+        public int GetCurrentBrightness()
+        {
+            return RunArg0RetInt("MODULEKEYBOARDBACKLIT_GetBrightnessCurrentLevel");
+        }
+
+        public int Initialise()
+        {
+            return RunArg0RetInt("MODULEKEYBOARDBACKLIT_Initialize");
+        }
+
+        public int SetBrightness(int brightness)
+        {
+            return RunArgIntRetInt("MODULEKEYBOARDBACKLIT_SetBrightnessLevel", brightness);
+        }
+
+        public int SetBrightnessWithoutSaving(int brightness)
+        {
+            return RunArgIntRetInt("MODULEKEYBOARDBACKLIT_SetBrightnessLevelWithoutSave", brightness);
+        }
+
+        public int GetMaximumBrightness()
+        {
+            return RunArg0RetInt("MODULEKEYBOARDBACKLIT_GetBrightnessMaxLevel");
+        }
+
+        public int GetStatus(int arg0)
+        {
+            return RunArgIntRetInt("MODULEKEYBOARDBACKLIT_GetStatus", arg0);
+        }
+
+        public bool GetALSExistence()
+        {
+            var exists = false;
+            if(RunArg0RetInt("MODULEKEYBOARDBACKLIT_GetALSExistence")==0)
+            {
+                exists = true;
+            }
+            return exists;
+        }
+
+        public int GetALSStatus()
+        {
+            return RunArg0RetInt("MODULEKEYBOARDBACKLIT_GetKBDALSStatus");
+        }
+
+        public void ToggleBacklight()
+        {
+           RunArg0RetInt("MODULEKEYBOARDBACKLIT_SetToggleBacklit");
+        }
+
+        public int GetKeboardType()
+        {
+            return RunArg0RetInt("MODULEKEYBOARDBACKLIT_GetType");
+        }
+
+        public int SetIdleLevel(int level)
+        {
+            return RunArgIntRetInt("MODULEKEYBOARDBACKLIT_SetIdleLevel", level);
+        }
+    }
+}
